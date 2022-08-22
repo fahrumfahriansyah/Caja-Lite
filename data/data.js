64dd165469edff1f12e7abebd8925ff9ac7a3ed9
fahrumfahriansyah <fahrumfahriansyah1@gmail.com>
2022-08-23 03:43:53 +0700

    menambahkan vidio dan music

--- data/data.js
+++ data/data.js
@@ -8,4 +8,24 @@ function dataGambar2(name) {
     const find = data.find((a) => a.tanda === name)
     return find
 }
-module.exports = { dataGambar, dataGambar2 }
+
+function dataVidio() {
+    const arr = [{ nama: "aot.mp4", tanda: 'Attack_of_titan' }, { nama: "trailer_demon.mp4", tanda: 'Demon_Slayer' }]
+    return arr
+}
+function dataVidio2(name) {
+    const data = dataVidio()
+    const find = data.find((a) => a.tanda === name)
+    return find
+}
+
+function dataMusic() {
+    const arr = [{ nama: "Crazy-Little-Thing-Called-Love.mp3", tanda: 'Crazy-Little-Thing-Called-Love' }, { nama: "hey_jude.mp3", tanda: 'hey_jude' }, { nama: "imagine.mp3", tanda: 'imagine' }, { nama: "Love-of-My-Life.mp3", tanda: 'Love-of-My-Life' }, { nama: "We-Are-The-Champions.mp3", tanda: 'We-Are-The-Champions' }, { nama: "yesterday.mp3", tanda: 'yesterday' }]
+    return arr
+}
+function dataMusic2(name) {
+    const data = dataMusic()
+    const find = data.find((a) => a.tanda === name)
+    return find
+}
+module.exports = { dataGambar, dataGambar2, dataVidio, dataVidio2, dataMusic, dataMusic2 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { dataGambar, dataGambar2 } = require('./data/data')
+const { dataGambar, dataGambar2, dataVidio, dataVidio2, dataMusic, dataMusic2 } = require('./data/data')
 //!set up
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -16,14 +16,27 @@ app.get('/gambar', (req, res) => {
 app.get('/gambar/:nama?', (req, res) => {
     const data = dataGambar2(req.params.nama)
     console.log(data);
-    res.render('index', { judul: `gambar-${data.tanda}`, dataGambar: dataGambar(), dataGambar2: dataGambar2(), rl: req.url, dataGambar2: data })
+    res.render('index', { judul: `gambar-${data.tanda}`, dataGambar: dataGambar(), rl: req.url, dataGambar2: data })
 
 })
 app.get('/vidio', (req, res) => {
-    res.render('index', { judul: 'vidio' })
+    res.render('vidio', { judul: `vidio`, dataVidio: dataVidio() })
+})
+
+app.get('/vidio/:name?', (req, res) => {
+    const data = dataVidio2(req.params.name)
+    console.log(data);
+    res.render('vidio', { judul: `vidio-${data.tanda}`, dataVidio: dataVidio(), rl: req.url, dataVidio2: data })
+
 })
 app.get('/music', (req, res) => {
-    res.render('index', { judul: 'music' })
+    res.render('music', { judul: 'music', dataMusic: dataMusic() })
+})
+app.get('/music/:namanya?', (req, res) => {
+    const data = dataMusic2(req.params.namanya)
+    console.log(data);
+    res.render('music', { judul: `music-${data.tanda}`, dataMusic: dataMusic(), rl: req.url, dataMusic2: data })
+
 })
 
 
